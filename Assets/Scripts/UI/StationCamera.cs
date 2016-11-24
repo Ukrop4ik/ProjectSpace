1e488030fdd56ac89707fd3e8fd60bc6a431a249
andrey.zaycev <dondrakone89@mail.ru>
2016-11-25 00:34:02 +0300

    Station Ui restile

---
+++ Assets/Scripts/UI/StationCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StationCamera : MonoBehaviour {
+
+    public GameObject target;
+
+    void Update()
+    {
+        if (target.transform.childCount > 0)
+        {
+           // transform.LookAt(target.transform.GetChild(0));
+            this.transform.position = new Vector3(target.transform.GetChild(0).position.x, 10, target.transform.GetChild(0).position.z);
+        }
+
+    }
+}
