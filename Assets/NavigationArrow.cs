6ba495bad7381705766eebdc878704e8c3f9b592
andrey.zaycev <dondrakone89@mail.ru>
2016-12-08 00:57:07 +0300

    Arrow

---
+++ Assets/NavigationArrow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigationArrow : MonoBehaviour {
+
+    public Transform player;
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (target == null) return;
+        var direction = (target.transform.position - player.transform.position);
+        direction.y = direction.z;
+        direction.z = 0;
+        var rotator = Quaternion.FromToRotation(Vector3.up, direction);
+        transform.rotation = rotator;
+    }
+}
